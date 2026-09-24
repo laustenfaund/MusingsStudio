85b3da052660b7092eae4e2a66e31c07f15f73ce
lausten faund <laustenfaund@gmail.com>
2026-09-24 00:44:31 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
    Paste the three values from Google Cloud here (see README.md, step 2).
    These are not secrets: they only work from your GitHub Pages address. */
 window.MUSINGS_CONFIG = {
-  GOOGLE_CLIENT_ID: "PASTE_CLIENT_ID_HERE.apps.googleusercontent.com",
-  GOOGLE_API_KEY: "PASTE_API_KEY_HERE",
-  GOOGLE_APP_ID: "PASTE_PROJECT_NUMBER_HERE"
+  GOOGLE_CLIENT_ID: "453781204970-v67pn1e31323noqignpl2b74db58doko.apps.googleusercontent.com",
+  GOOGLE_API_KEY: "AIzaSyAm-rrX_dYVbW7-2WYnYNpJU_Ym0aev9_E",
+  GOOGLE_APP_ID: "453781204970"
 };
